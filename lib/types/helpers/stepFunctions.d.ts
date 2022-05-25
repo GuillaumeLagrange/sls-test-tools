a1a3ee44bbda672cdb88670dfbf5e256ac1b82c4
Guillaume Lagrange <guillaumelag@theodo.fr>
2022-05-25 15:58:59 +0200

    add startExecution function

--- lib/types/helpers/stepFunctions.d.ts
+++ lib/types/helpers/stepFunctions.d.ts
@@ -4,6 +4,7 @@ export default class StepFunctions {
     allStateMachines: AWSStepFunctions.ListStateMachinesOutput | undefined;
     init(): Promise<void>;
     static build(): Promise<StepFunctions>;
+    startExecution(stateMachineName: string, input: unknown): Promise<AWSStepFunctions.StartExecutionOutput>;
     runExecution(stateMachineName: string, input: unknown): Promise<AWSStepFunctions.DescribeExecutionOutput>;
     obtainStateMachineArn(stateMachineName: string): Promise<string>;
     obtainExecutionArn(StateMachineArn: string): Promise<string>;
